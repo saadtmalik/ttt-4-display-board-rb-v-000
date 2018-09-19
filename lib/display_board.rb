f4b41823be402cb7e13234205825e300da9aabaf
Saad Malik <saad.t.malik@gmail.com>
2018-09-19 06:46:00 +0000

    automatically backed up by learn

--- lib/display_board.rb
+++ lib/display_board.rb
@@ -1,5 +1,6 @@
+board = ("   ","   ","   ","   "," X ","   ","   ","   ","   ")
+
 def display_board(board)
-  board = ("   ","   ","   ","   "," X ","   ","   ","   ","   ")
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
   puts " #{board[3]} | #{board[4]} | #{board[5]} "
